86586c9bac145442989e44ff26ecea57c201ef6f
VengateshRK <39152848+VengateshRK@users.noreply.github.com>
2021-09-07 15:33:56 +0530

    Update myFindIntersection.java

--- myFindIntersection.java
+++ myFindIntersection.java
@@ -5,8 +5,6 @@ public class myFindIntersection {
 	public static void main(String[] args) {
 		int [] arr1= {3,2,11,4,6,7};
 		int [] arr2= {1,2,8,4,9,7};
-		Arrays.sort(arr1);
-		Arrays.sort(arr2);
 		for (int i=0; i< arr1.length ; i++) {
 			for (int j=0; j< arr2.length ; j++) {
 				
